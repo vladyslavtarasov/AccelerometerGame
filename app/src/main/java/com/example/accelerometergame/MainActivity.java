25382f1dd7cad7554be114bb7c13a64b68643259
Vladyslav Tarasov <vladyslavtarasov02@gmail.com>
2023-06-08 08:10:46 +0100

    Set requested orientation to portrait

--- app/src/main/java/com/example/accelerometergame/MainActivity.java
+++ app/src/main/java/com/example/accelerometergame/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.accelerometergame;
 
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -15,6 +16,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 
         startButton = findViewById(R.id.start_button);
         startButton.setOnClickListener(new View.OnClickListener() {
